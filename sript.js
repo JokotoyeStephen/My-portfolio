68fccfea00d2068488044a38ce349fb0f1ca1edf
JokotoyeStephen <jokotoyestephen2023@gmail.com>
2025-10-28 13:35:35 +0100

    working on the respomsiveness of my portfolio

--- sript.js
+++ sript.js
@@ -1,4 +1,11 @@
-
+  window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
 
 const menuToggle = document.getElementById('menu-toggle');
     const mobileMenu = document.getElementById('mobile-menu');
